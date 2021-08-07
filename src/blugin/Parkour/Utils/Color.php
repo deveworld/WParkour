c9130197ff7eeacbe322aab0eb0a0d2d35c75fcd
World <simworld415@gmail.com>
2021-08-07 21:28:31 +0900

    del codes

--- src/blugin/Parkour/Utils/Color.php
+++ src/blugin/Parkour/Utils/Color.php
@@ -7,31 +7,6 @@ use pocketmine\lang\TextContainer;
 class Color extends TextContainer {
     public const COLOR_CODE = "\xc2\xa7";
 
-    // from https://github.com/pmmp/PocketMine-MP/blob/stable/src/pocketmine/utils/TextFormat.php#L53
-    public const BLACK = "0";
-    public const DARK_BLUE = "1";
-    public const DARK_GREEN = "2";
-    public const DARK_AQUA = "3";
-    public const DARK_RED = "4";
-    public const DARK_PURPLE = "5";
-    public const GOLD = "6";
-    public const GRAY = "7";
-    public const DARK_GRAY = "8";
-    public const BLUE = "9";
-    public const GREEN = "a";
-    public const AQUA = "b";
-    public const RED = "c";
-    public const LIGHT_PURPLE = "d";
-    public const YELLOW = "e";
-    public const WHITE = "f";
-
-    public const OBFUSCATED = "k";
-    public const BOLD = "l";
-    public const STRIKETHROUGH = "m";
-    public const UNDERLINE = "n";
-    public const ITALIC = "o";
-    public const RESET = "r";
-
     protected string $code;
     public static string $prefix, $explain, $button, $warning, $error;
 
